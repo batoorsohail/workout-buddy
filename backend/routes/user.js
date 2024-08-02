c673ed8e8bf69552a5c4f8224c3d0aa03820d088
Sohail Batoor <sohailshahidi1435@gmail.com>
2024-08-02 10:11:29 +0430

    Add userController.js file

--- backend/routes/user.js
+++ backend/routes/user.js
@@ -1,11 +1,14 @@
 const express = require('express')
 
-const router = express.Router
+// controller functions
+const { loginUser, signupUser } = require('../controllers/userController')
+
+const router = express.Router()
 
 // login route
-router.post('/login', () => {})
+router.post('/login', loginUser)
 
 // signup route
-router.post('/signup', ()=> {})
+router.post('/signup', signupUser)
 
 module.exports = router
