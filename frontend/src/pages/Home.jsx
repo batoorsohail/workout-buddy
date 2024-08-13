d1f54c9740929c26e43204e21f243d92b1837f8c
Sohail Batoor <sohailshahidi1435@gmail.com>
2024-08-13 17:46:09 +0430

    Add Authorization key

--- frontend/src/pages/Home.jsx
+++ frontend/src/pages/Home.jsx
@@ -1,5 +1,6 @@
 import { useEffect } from "react";
 import { useWorkoutsContext } from "../hooks/useWorkoutsContext";
+import { useAuthContext } from "../hooks/useAuthContext";
 
 // components
 import WorkoutDetails from "../components/WorkoutDetails";
@@ -7,11 +8,14 @@ import WorkoutForm from "../components/WorkoutForm";
 
 const Home = () => {
   const { workouts, dispatch } = useWorkoutsContext();
+  const { user } = useAuthContext();
 
   useEffect(() => {
     const fetchWorkouts = async () => {
       try {
-        const response = await fetch("http://localhost:4000/api/workouts");
+        const response = await fetch("http://localhost:4000/api/workouts", {
+          headers: {'Authorization': `Bearer ${user.token}`}
+        });
         const json = await response.json()
 
         if (response.ok) {
@@ -22,7 +26,9 @@ const Home = () => {
       }
     };
 
-    fetchWorkouts();
+    if (user) {
+      fetchWorkouts();
+    }
   }, [dispatch]);
 
   return (
